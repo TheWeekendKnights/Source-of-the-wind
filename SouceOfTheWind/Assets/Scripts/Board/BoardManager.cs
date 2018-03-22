03a1ac198cca40bae4b6e725a6a9b19f72f25b1a
moon <moonsin@outlook.com>
2018-03-22 23:53:56 +0000

    完成第一幕场景布置

---
+++ SouceOfTheWind/Assets/Scripts/Board/BoardManager.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public abstract class BoardManager : MonoBehaviour {
+	public float[,] floorYArray;
+	public string[,] lastTileArray;
+	public int[,] floorHightArray;
+
+	public GameObject[] grassfloorTiles;
+	public GameObject[] waterfloorTiles;
+	public GameObject[] lowfloorTiles;
+	public GameObject[] sandfloorTiles;
+	public GameObject[] stonefloorTiles; 
+	public GameObject[] CoverfloorTiles; 
+
+	protected Transform boardHolder;
+	protected Dictionary<string, string> HeightDic = new Dictionary<string, string>();
+
+
+
+	public void updateFloorYArray(int x, int y, float high){
+		floorYArray [x, y] = high;
+	}
+
+	public void updateLastTileArray(int x, int y, string lastTile){
+		lastTileArray [x, y] = lastTile;
+	}
+
+	public void updateFloorHightArray(int x, int y, int hight){
+		floorHightArray [x, y] = floorHightArray [x, y] + hight;
+	}
+
+	//用来覆盖新的砖块素材
+	public Vector3 Overlay(int x, int y, GameObject toInstantiate){
+		Vector3 newVec = new Vector3();
+		newVec.x = x + y;
+
+		if (toInstantiate.tag != lastTileArray [x, y]) {
+			newVec.y = floorYArray [x, y] + 1.0f;
+
+		} else if (toInstantiate.tag == "lowFloor") {
+			newVec.y = floorYArray [x, y] + 0.5f;
+		} else {
+			newVec.y = floorYArray [x, y] + 1.5f;
+		}
+		floorYArray [x, y] = newVec.y;
+		lastTileArray [x, y] = toInstantiate.tag;
+		floorHightArray [x, y] += Int32.Parse (HeightDic [toInstantiate.tag]);
+
+		newVec.z = 0f;
+
+		return newVec;
+	}
+
+	public Vector3 CoverTile(int x, int y){
+		Vector3 newVec = new Vector3();
+		newVec.x = x + y;
+		if (lastTileArray [x, y] == "lowFloor") {
+			newVec.y = floorYArray [x, y] - 0.5f;
+		} else {
+			newVec.y = floorYArray [x, y];
+		}
+
+		return newVec;
+	}
+
+	public Vector3 TransFromWorldToISO(Vector3 vec, GameObject toInstantiate){
+		Vector3 newVec = new Vector3();
+
+		newVec.x = vec.x + vec.y;
+		newVec.y = (vec.y - vec.x) / 2;
+
+		if (toInstantiate.tag == "lowFloor") {
+			newVec.y -= 0.5f;
+		}
+
+		return newVec;
+	}
+
+	protected int setFloor(GameObject toInstantiate, int LayerOrder, int x, int y, bool FirstFloor){
+		//Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
+		//GameObject toInstantiate = grassfloorTiles [Random.Range (0, grassfloorTiles.Length)];
+
+		toInstantiate.GetComponent<SpriteRenderer> ().sortingOrder = LayerOrder;
+		LayerOrder += 1;
+		Vector3 ISOVec = new Vector3 ();
+		if (FirstFloor) {
+			ISOVec = TransFromWorldToISO (new Vector3 (x, y, 0f), toInstantiate);
+
+			int height;
+			height = Int32.Parse (HeightDic [toInstantiate.tag]);
+			updateFloorYArray(x,y,TransFromWorldToISO (new Vector3 (x, y, 0f), toInstantiate).y);
+			updateLastTileArray (x, y, toInstantiate.tag);
+			updateFloorHightArray(x,y,height);
+
+		} else {
+			ISOVec = Overlay (x, y, toInstantiate);
+
+		}
+		GameObject instance =
+			Instantiate (toInstantiate, ISOVec, Quaternion.identity) as GameObject;
+
+
+
+
+
+
+		//Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
+		instance.transform.SetParent (boardHolder);
+
+		return LayerOrder;
+	}
+
+
+	protected int setCover(GameObject toInstantiate,int LayerOrder, int x, int y){
+
+		toInstantiate.GetComponent<SpriteRenderer> ().sortingOrder = LayerOrder;
+		LayerOrder += 1;
+
+		Vector3 ISOVec = new Vector3 ();
+		ISOVec = CoverTile (x, y);
+
+		GameObject instance =
+			Instantiate (toInstantiate, ISOVec, Quaternion.identity) as GameObject;
+
+		instance.transform.SetParent (boardHolder);
+
+		return LayerOrder;
+	}
+
+
+	protected void Awake(){
+		HeightDic.Add ("lowFloor", "1");
+		HeightDic.Add ("normalFloor", "3");
+	}
+		
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SouceOfTheWind/Assets/Scripts/Board/FirstBoardManager.cs
@@ -0,0 +1,322 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public class FirstBoardManager : BoardManager {
+
+	public int columns = 25; 										//Number of columns in our game board.
+	public int rows = 20;											//Number of rows in our game board.
+
+
+
+	void Awake(){
+		floorYArray = new float[25, 20];
+		lastTileArray = new string[25, 20];
+		floorHightArray = new int[25, 20];
+		base.Awake ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+
+
+	int layerOneRender(int LayerOrder,Transform boardHolder){
+		for (int y = rows - 1; y >= 0; y--) {
+			//Loop along y axis, starting from -1 to place floor or outerwall tiles.
+			for (int x = 0; x < columns; x++) {
+				GameObject toInstantiate = sandfloorTiles [1];
+
+				//渲染水
+				if (( (y == 0 || y==1) && x >= 5 && x <= 12) ||
+					( y==2 && x>=4 && x<= 11) ||
+					( y==3 && x>=4 && x<=9) ||
+					( y==4 && x>=4 && x<=8) ||
+					( y>4 && y<= 9 && x<= 11-y) ||
+					( y==10 && x<=2) ||
+					( y>=11 && y<=12 && x<=1)) {
+					toInstantiate = waterfloorTiles [0];
+				}
+				//渲染植被土
+				else if(
+					((y == 0 || y==1) &&(x<=4 || x>=13)) ||
+					( y ==2 &&( x<=3 || x>=12)) ||
+					( y ==3 &&(x<=3 || x>=10)) ||
+					( y ==4 && (x<=3 || x>=9)) ||
+					( y ==5 && x>11-y)){
+					toInstantiate = grassfloorTiles [4];
+				}
+
+				//渲染洞
+				else if (
+					(y == 11 && x>= 13 && x<=14) ||
+					(y == 12 && x>= 10 && x<=14) ||
+					(y == 13 && x>=9 && x<=15) ||
+					(y == 14 && x>=9 && x<=14) ||
+					((y == 15||y==16) && x>=8 && x<=14)
+				){
+					toInstantiate = lowfloorTiles [0];
+				}
+						
+
+				else{
+					toInstantiate = sandfloorTiles [1];
+				}
+
+				toInstantiate.GetComponent<SpriteRenderer> ().sortingLayerName = "underFloor";
+
+				LayerOrder = setFloor (toInstantiate, LayerOrder,x ,y, true);
+
+			}
+		}
+
+		return 0;
+	}
+
+
+
+	int layerTwoRender(int LayerOrder){
+		for (int y = rows - 1; y >= 0; y--) {
+			//Loop along y axis, starting from -1 to place floor or outerwall tiles.
+			for (int x = 0; x < columns; x++) {
+				GameObject toInstantiate = sandfloorTiles [1];
+
+				//渲染水
+				if (( (y == 0 || y==1) && x >= 5 && x <= 12) ||
+					( y==2 && x>=4 && x<= 11) ||
+					( y==3 && x>=4 && x<=9) ||
+					( y==4 && x>=4 && x<=8) ||
+					( y>4 && y<= 9 && x<= 11-y) ||
+					( y==10 && x<=2) ||
+					( y>=11 && y<=12 && x<=1)) {
+					toInstantiate = null;
+				}
+
+				//渲染植被土
+				else if (
+					( y == 0  && (x <= 4 || x >= 13)) ||
+					( y == 1 && ( x<=3  || x>=13))	||
+					(y == 2 && ((x <= 2 && x>=1) || x > 14)) ||
+					(y == 3 &&  x > 14) ||
+					(y == 4 && x > 15) ||
+						(y == 5 && x >15)) {
+					toInstantiate = lowfloorTiles [1];
+				}
+
+				//渲染洞
+				else if (
+					(y == 11 && x>= 13 && x<=14) ||
+					(y == 12 && x>= 10 && x<=14) ||
+					(y == 13 && x>=9 && x<=15) ||
+					(y == 14 && x>=9 && x<=14) ||
+					(y == 15 && x>=8 && x<=14)
+				) {
+					toInstantiate = null;
+				}
+
+				else if( ( y == 2 && x ==0) ||
+					( y==3 && x<=2)||
+					(y == 4 && x <=3)){
+					toInstantiate = stonefloorTiles[0];
+				}
+
+				else if (y==16 && x>=8 && x<=14){
+					toInstantiate = sandfloorTiles [2];
+				}
+
+				else {
+					toInstantiate = lowfloorTiles [2];
+				}
+
+
+				if (toInstantiate != null) {
+					toInstantiate.GetComponent<SpriteRenderer> ().sortingLayerName = "floor";
+
+					LayerOrder = setFloor (toInstantiate, LayerOrder, x, y, false);
+				}
+			}
+		}
+
+		layerTwoCoverRender (LayerOrder);
+		print (floorYArray [0, 0]);
+		return 0;
+	}
+		
+
+	int layerTwoCoverRender(int LayerOrder){
+		for (int y = rows - 1; y >= 0; y--) {
+			//Loop along y axis, starting from -1 to place floor or outerwall tiles.
+			for (int x = 0; x < columns; x++) {
+				GameObject toInstantiate = null;
+
+				//渲染完全绿草
+				if (( y == 0  && (x <= 4 || x >= 13)) ||
+					( y == 1 && ( x<=3 || x>=13))	||
+					(y == 2 && ((x <= 2 && x>0) || x > 14)) ||
+					(y == 3 &&  x > 14) ||
+					(y == 4 &&  x > 15) ||
+					(y == 5 && x >15) ||
+					(y == 6 && (x>=18 && x<=20))
+				) {
+					toInstantiate = CoverfloorTiles [0];
+				}
+
+				//渲染浓绿草
+
+				else if((y==1 && x==4) ||
+					( y == 2 &&(x == 3 || (x>=12 && x<=13))) ||
+					( y == 3 && x==3) ||
+					( y == 6 && x>=20)
+				){
+					toInstantiate = CoverfloorTiles [2];
+				}
+
+				if (toInstantiate != null) {
+					toInstantiate.GetComponent<SpriteRenderer> ().sortingLayerName = "floor";
+
+					LayerOrder = setCover (toInstantiate, LayerOrder, x, y);
+				}
+
+
+			}
+		}
+		return LayerOrder;
+	}
+
+
+	int layerThreeRender(int LayerOrder){
+		for (int y = rows - 1; y >= 0; y--) {
+			//Loop along y axis, starting from -1 to place floor or outerwall tiles.
+			for (int x = 0; x < columns; x++) {
+				GameObject toInstantiate = null;
+
+				//渲染正常石头块
+				if(( y == 3 && x == 1) ||
+					( y == 4 && x<=2) ||
+					( y==17 &&(x>=10 && x<=20 && x!=14)) ||
+					( y == 18 &&( x>=10 && x<=21)) ||
+					( y == 19 && x>=10)
+				){
+					toInstantiate = stonefloorTiles[0];
+				}
+
+				//渲染矮石头块
+				else if ((y == 16 && (x>=10 && x<=17) ||
+					( y == 17 &&(x==9 || x==14 || x==21 ||x==22)) ||
+					( y == 18 && x>=21)
+				)
+				){
+					toInstantiate = lowfloorTiles [0];
+				}
+
+				if (toInstantiate != null) {
+					toInstantiate.GetComponent<SpriteRenderer> ().sortingLayerName = "higherFloor";
+
+					LayerOrder = setFloor (toInstantiate, LayerOrder, x, y, false);
+				}
+			}
+		}
+
+		return LayerOrder;
+	}
+
+	int layerFourRender(int LayerOrder){
+		for (int y = rows - 1; y >= 0; y--) {
+			//Loop along y axis, starting from -1 to place floor or outerwall tiles.
+			for (int x = 0; x < columns; x++) {
+				GameObject toInstantiate = null;
+
+				//渲染正常石头块
+				if((y==4 && x==1) ||
+					(y==18 &&( x>=11 && x<=20)) ||
+					(y == 19 &&( x>=10 && x<=23))
+				
+				){
+					toInstantiate = stonefloorTiles[0];
+				}
+
+				else if ( y== 18 && x==22){
+					toInstantiate = lowfloorTiles [0];
+				}
+
+				if (toInstantiate != null) {
+					toInstantiate.GetComponent<SpriteRenderer> ().sortingLayerName = "higherFloor";
+
+					LayerOrder = setFloor (toInstantiate, LayerOrder, x, y, false);
+				}
+
+			}
+		}
+		return LayerOrder;
+	}
+
+	int layerFiveRender(int LayerOrder){
+		for (int y = rows - 1; y >= 0; y--) {
+			//Loop along y axis, starting from -1 to place floor or outerwall tiles.
+			for (int x = 0; x < columns; x++) {
+				GameObject toInstantiate = null;
+
+				if((y ==18 && (x>=13 && x<=18)) ||
+					(y== 19 && (x>=11 && x<=19 && x!=15))
+				){
+					toInstantiate = stonefloorTiles[0];
+				}
+
+				if (toInstantiate != null) {
+					toInstantiate.GetComponent<SpriteRenderer> ().sortingLayerName = "higherFloor";
+
+					LayerOrder = setFloor (toInstantiate, LayerOrder, x, y, false);
+				}
+				
+
+			}
+		}
+		return LayerOrder;
+	}
+
+
+	void BoardRender(Transform boardHolder){
+
+		int LayerOrder = 0;
+
+
+		for(int layerIndex = 0; layerIndex < 5;layerIndex++){
+		//Loop along x axis, starting from -1 (to fill corner) with floor or outerwall edge tiles.
+			if (layerIndex == 0) {
+				LayerOrder = layerOneRender (LayerOrder, boardHolder);
+			} else if (layerIndex == 1) {
+				LayerOrder = layerTwoRender (LayerOrder);
+			} else if (layerIndex == 2) {
+				LayerOrder = layerThreeRender (LayerOrder);
+			} else if (layerIndex == 3) {
+				LayerOrder = layerFourRender (LayerOrder);
+			} else if (layerIndex == 4) {
+				LayerOrder = layerFiveRender (LayerOrder);
+			}
+
+		}
+	}
+
+	void BoardSetup ()
+	{
+		//Instantiate Board and set boardHolder to its transform.
+		boardHolder = new GameObject ("Board").transform;
+		BoardRender (boardHolder);
+		//print (floorHightArray [1, 14]);
+
+	}
+
+	public void SetupScene (){
+		BoardSetup ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
